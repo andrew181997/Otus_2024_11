f9b1c0e79881cf5dd0cab9471bbb6c0c2d27c163
andrei_demidov1897 <forvard199718@gmail.com>
2024-12-14 00:35:20 +0300

    first iteration hw2

---
+++ src/rectangle.py
@@ -0,0 +1,19 @@
+from figure import Figure
+
+
+class Rectangle(Figure):
+    def __init__(self, side_a, side_b):
+        self.side_a = side_a
+        self.side_b = side_b
+        super(Rectangle, self).__init__()
+        self.validate_numeric(self.side_a, self.side_b)
+
+    @property
+    def get_area(self):
+        result = self.side_a * self.side_b
+        return round(result, 2)
+
+    @property
+    def get_perimeter(self):
+        result = 2 * (self.side_a + self.side_b)
+        return round(result, 2)
